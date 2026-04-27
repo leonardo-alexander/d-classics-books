d251da48379ab12361283dc8ce5c4720f4071b32
Leonardo Alexander Wijaya <140410087+leonardo-alexander@users.noreply.github.com>
2026-04-27 13:04:47 +0700

    seed books

--- app/src/main/java/com/example/dclassicsbooks/BooksActivity.java
+++ app/src/main/java/com/example/dclassicsbooks/BooksActivity.java
@@ -28,11 +28,11 @@ public class BooksActivity extends AppCompatActivity {
         bookList.add(new Book(
                 "Meditations",
                 "Marcus Aurelius",
-                "A collection of personal writings from a Roman emperor reflecting on Stoic philosophy. It focuses on self-discipline, resilience, and accepting things beyond your control while striving to live a virtuous life",
+                "A collection of personal writings from a Roman emperor reflecting on Stoic philosophy. It focuses on self-discipline, resilience, and accepting things beyond your control while striving to live a virtuous life.",
                 "Non-Fiction",
                 "0140449337",
                 319,
-                "April 27, 2006",
+                "27 Apr. 2006",
                 R.drawable.img_meditations
         ));
 
@@ -43,8 +43,96 @@ public class BooksActivity extends AppCompatActivity {
                 "Non-Fiction",
                 "9124238341",
                 580,
-                "June 24, 2023",
+                "24 Jun. 2023",
                 R.drawable.img_the_subtle_art_of_not_giving_a_f
+        ));
+
+        bookList.add(new Book(
+                "Beyond Good and Evil",
+                "Friedrich Nietzsche",
+                "A philosophical critique of traditional morality. Nietzsche challenges the idea of absolute good and evil, encouraging independent thinking and questioning societal norms.",
+                "Non-Fiction",
+                "014044923X",
+                240,
+                "27 Feb. 2003",
+                R.drawable.img_beyond_good_and_evil
+        ));
+
+        bookList.add(new Book(
+                "Letters To Milena",
+                "Franz Kafka",
+                "A collection of deeply emotional letters Kafka wrote to Milena Jesenská. It reveals his thoughts on love, anxiety, and his inner struggles in a raw and personal way.",
+                "Non-Fiction",
+                "1784874000",
+                272,
+                "6 Dec. 2018",
+                R.drawable.img_letters_to_milena
+        ));
+
+        bookList.add(new Book(
+                "The Art of Loving",
+                "Erich Fromm",
+                "Explores love as a skill that requires effort, discipline, and understanding. Fromm argues that true love is not just a feeling but an active practice rooted in care, responsibility, and respect.",
+                "Non-Fiction",
+                "9781480402447",
+                142,
+                "26 Feb. 2013",
+                R.drawable.img_the_art_of_loving
+        ));
+
+        bookList.add(new Book(
+                "101 Essays That Will Change The Way You Think",
+                "Brianna Wiest",
+                "A collection of thought-provoking essays about mindset, self-awareness, and personal growth. It challenges limiting beliefs and encourages deeper reflection on how you see yourself and the world.",
+                "Non-Fiction",
+                "1945796065",
+                448,
+                "7 Nov. 2018",
+                R.drawable.img_101_essays_that_will_change_the_way_you_think
+        ));
+
+        bookList.add(new Book(
+                "White Nights",
+                "Fyodor Dostoyevsky",
+                "A lonely dreamer in St. Petersburg meets a young woman and falls in love over a few nights. It’s a bittersweet story about hope, loneliness, and unfulfilled love.",
+                "Fiction",
+                "6349606558",
+                88,
+                "3 Sep. 2025",
+                R.drawable.img_white_nights
+        ));
+
+        bookList.add(new Book(
+                "No Longer Human",
+                "Osamu Dazai",
+                "A deeply personal story of a man who feels disconnected from society. It explores alienation, depression, and the struggle to understand what it means to be human.",
+                "Fiction",
+                "0811204812",
+                272,
+                "1 Feb. 1973",
+                R.drawable.img_no_longer_human
+        ));
+
+        bookList.add(new Book(
+                "The Stranger",
+                "Albert Camus",
+                "A lonely dreamer in St. Petersburg meets a young woman and falls in love over a few nights. It’s a bittersweet story about hope, loneliness, and unfulfilled love.",
+                "Fiction",
+                "6349606558",
+                88,
+                "3 Sep. 2025",
+                R.drawable.img_white_nights
+        ));
+
+        bookList.add(new Book(
+                "No Longer Human",
+                "Osamu Dazai",
+                "A deeply personal story of a man who feels disconnected from society. It explores alienation, depression, and the struggle to understand what it means to be human.",
+                "Fiction",
+                "0811204812",
+                272,
+                "1 Feb. 1973",
+                R.drawable.img_no_longer_human
         ));
 
         adapter = new BookAdapter(bookList, book -> {
